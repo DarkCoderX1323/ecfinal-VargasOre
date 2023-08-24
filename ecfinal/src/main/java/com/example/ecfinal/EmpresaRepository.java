7438315f155ea387b856326471b82f1e41fe5842
DarkCoder1323 <rayvargas1323@gmail.com>
2023-08-24 15:17:13 -0500

    Update

--- ecfinal/src/main/java/com/example/ecfinal/EmpresaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ecfinal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-}
-
-
--- ecfinal/src/main/java/com/example/ecfinal/PersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ecfinal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
-
-
