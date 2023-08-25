a297ed048bab8324f587cebc83f223218106746b
DarkCoder1323 <rayvargas1323@gmail.com>
2023-08-25 07:42:12 -0500

    Update

---
+++ frontend-crud/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Marcas from './components/Marcas';
+import Vehiculos from './components/Vehiculos';
+
+function App() {
+  return (
+    <div className="App">
+      <Marcas />
+      <Vehiculos />
+    </div>
+  );
+}
+
+export default App;
+
